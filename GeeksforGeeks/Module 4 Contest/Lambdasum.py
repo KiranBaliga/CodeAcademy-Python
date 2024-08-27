b5b42839c8721ac793b9ce26bab1ec9ffa6e49c3
Kiran Baliga B <68471170+KiranBaliga@users.noreply.github.com>
2024-08-27 14:06:40 +0530

    Create Lambdasum.py

---
+++ GeeksforGeeks/Module 4 Contest/Lambdasum.py
@@ -0,0 +1,46 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+# } Driver Code Ends
+#User function Template for python3
+
+
+#Initial Template for Python 3
+
+#User function Template for python3
+
+##Write your lambda function here that will be assigned to sum
+sum = lambda X, Y: X + Y
+
+#Driver Code Starts
+def main():
+    testcases = int(input()) # number of testcases
+    while testcases > 0:
+        X = int(input())
+        Y = int(input())
+
+
+
+
+#{ 
+ # Driver Code Starts.
+
+
+def main():
+    testcases = int(input()) #testcases
+    while(testcases>0):
+        X = int(input())
+        Y = int(input())
+        
+        print(sum(X, Y))
+        
+        testcases -= 1
+        
+
+
+if __name__=='__main__':
+    main()
+# } Driver Code Ends
